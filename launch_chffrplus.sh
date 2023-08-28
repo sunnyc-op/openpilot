65f95781262a80269f1c440b748cf02be3ab684a
sunnyc-op <95874127+sunnyc-op@users.noreply.github.com>
2023-08-27 17:06:39 -0700

    Update launch_chffrplus.sh

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -126,6 +126,7 @@ function launch {
   fi
   
   python ./selfdrive/car/hyundai/values.py > /data/params/d/SupportedCars
+  python ./selfdrive/car/gm/values.py > /data/params/d/SupportedCars_gm
   
   dongleid=`cat /data/params/d/DongleId`
 
